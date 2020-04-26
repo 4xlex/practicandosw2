1f45357bc004d33ea214973507f2a5e522af962a
lewis-coder <57689615+lewis-coder@users.noreply.github.com>
2020-04-26 15:13:19 -0500

    primer comit

---
+++ src/test/java/com/example/lab2pc/Lab2pcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2pc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2pcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
